2354d821c1c0b6c826483782a1a8458cfc8c022b
mii-chan <miichan.maple.apps@gmail.com>
2018-05-14 16:08:34 +0900

    Formatting

--- app/src/main/java/com/example/miichan/omikujiappkotlin/MainActivity.kt
+++ app/src/main/java/com/example/miichan/omikujiappkotlin/MainActivity.kt
@@ -27,7 +27,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (requestCode == ResultActivity.REQUEST_CODE && resultCode == Activity.RESULT_OK) {
-
             // 前回のおみくじの結果をタイトルに表示する
             if (data != null && data.getIntExtra(ResultActivity.EXTRA_NAME_RES, -1) != -1) {
                 supportActionBar?.title = "前回は%sでした"
