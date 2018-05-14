e6aa842cd3d9132c4dc6604231f2ebf679430e43
mii-chan <miichan.maple.apps@gmail.com>
2018-05-14 13:01:42 +0900

    Start ResultActivity

--- app/src/main/java/com/example/miichan/omikujiappkotlin/MainActivity.kt
+++ app/src/main/java/com/example/miichan/omikujiappkotlin/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.miichan.omikujiappkotlin
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 「おみくじを引く」を押したら、ResultActivityを開始する
+        startOmikujiButton.setOnClickListener { startResultActivity() }
+    }
+
+
+    private fun startResultActivity() {
+        // Intentの作成
+        val intent = Intent(this, ResultActivity::class.java)
+
+        // Activityの開始
+        startActivity(intent)
     }
 }
